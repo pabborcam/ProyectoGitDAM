39f421221f2bdd83abeea984ecf7ffcacae07a43
pabborcam <pabborcam@orm4-10375355>
2026-02-05 19:54:45 +0100

    Guardando el readme.md

--- ProyectoGitDAM/src/test.java
+++ ProyectoGitDAM/src/test.java
@@ -1,4 +1,10 @@
 
 public class test {
-
+	public static void main(String[] ars) {
+		System.out.println("Estamos prácticando supongo...");
+		
+		int var1 = 4;
+		int var2 = 4;
+		System.out.println(var1 + " + " + var2 + " = " + (var1 + var2));
+	}
 }
